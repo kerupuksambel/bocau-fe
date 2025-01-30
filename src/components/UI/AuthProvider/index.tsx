2cadec3d2f8aa959101858251f03966810cb93df
Ammar Alifian Fahdan <kerupuksambel.2000@gmail.com>
2025-01-30 13:27:18 +0700

    feat: persistent storage

--- src/components/UI/AuthProvider/index.tsx
+++ src/components/UI/AuthProvider/index.tsx
@@ -3,15 +3,24 @@ import { useNavigate } from "react-router-dom"
 import fs from 'fs'
 import jwt from 'jsonwebtoken'
 
-const JWT_PUBLIC_KEY = fs.readFileSync('./public.pem')
+const JWT_PUBLIC_KEY = fs.readFileSync('./.keys/public.pem', 'utf-8')
+
 const AuthProvider = () => {
     const auth = useAuthStore()
     const navigate = useNavigate()
+    const destroyAuth = () => {
+        auth.setAuth({isAuthenticated: false, address: '', token: ''})
+    }
     
     if(!auth.auth.isAuthenticated) {
+        destroyAuth();
         navigate('/')
     }
 
     // Check if auth is still valid
-    // const decodedJWT = jwt.decode() 
+    const verifiedJWT = jwt.verify(auth.auth.token, JWT_PUBLIC_KEY, {algorithms: ['RS256']})
+    if(!verifiedJWT) {
+        destroyAuth();
+        navigate('/')
+    }
 }
--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 interface AuthProps {
     isAuthenticated: boolean,
@@ -11,17 +12,38 @@ interface AuthStoreProps {
     setAuth: (auth: AuthProps) => void
 }
 
-const useAuthStore = create<AuthStoreProps>((set) => ({
-    auth: {
-        isAuthenticated: false,
-        address: '',
-        token: '',
-    },
-    setAuth: (newAuth) => {
-        set(() => ({
-            auth: newAuth,
-        }));
-    },
-}));
+// const useAuthStore = create<AuthStoreProps>((set) => ({
+//     auth: {
+//         isAuthenticated: false,
+//         address: '',
+//         token: '',
+//     },
+//     setAuth: (newAuth) => {
+//         set(() => ({
+//             auth: newAuth,
+//         }));
+//     },
+// }));
+
+const useAuthStore = create<AuthStoreProps>()(
+    persist(
+        (set, _get) => ({
+            auth: {
+                isAuthenticated: false,
+                address: '',
+                token: '',
+            },
+            setAuth: (newAuth) => {
+                set(() => ({
+                    auth: newAuth,
+                }));
+            },
+        }),
+        {
+            name: 'auth-storage',
+            storage: createJSONStorage(() => localStorage),
+        }
+    )
+);
 
 export default useAuthStore
